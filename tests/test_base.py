66426afcd991175637b679edd03f8d30367e4b13
adamjackowski <adamus95@vp.pl>
2025-03-15 21:57:27 +0100

    adjust class to pylint hints

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,28 +1,33 @@
-import os, sys, inspect
+"""Base Test Module"""
+from pathlib import Path
+import json
+import os
+import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-import pytest
-import json
-from datetime import datetime
-import json
-from pathlib import Path
+
 
 class TestBase:
-        
+    """Base Test Class"""
     @staticmethod
     def data():
-        with open(Path(f"{currentdir}/config.json"),"r+") as config_file:
+        """Method for opening json file"""
+        with open(file=Path(f"{currentdir}/config.json"),
+                  mode="r+",
+                  encoding="utf-8") as config_file:
             data = json.load(config_file)
             config_file.close()
         return data
 
     @staticmethod
     def update_data(key, value):
-        with open(Path(f"{currentdir}/config.json"),"r+") as config_file:
+        """Method for updating json file"""
+        with open(file=Path(f"{currentdir}/config.json"),
+                  mode="r+",
+                  encoding="utf-8" ) as config_file:
             data = json.load(config_file)
             data[key] = value
             config_file.seek(0)
             json.dump(data, config_file, indent=4)
             config_file.truncate()
         return True
-        
